64177c13520762dd0819a43c35bef7b70657a7db
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2021-12-30 09:13:51 +0500

    Bug fix: removed unnecessary test method

--- app/Repository/TwitchRepositoryInterface.php
+++ app/Repository/TwitchRepositoryInterface.php
@@ -7,6 +7,5 @@ use Illuminate\Database\Eloquent\Collection;
 interface TwitchRepositoryInterface
 {
     public function init($url, $headers);
-    public function getTopLiveStreams();
     public function fetchListOfTags();
 }
