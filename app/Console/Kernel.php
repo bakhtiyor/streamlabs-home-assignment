fe8fcba0469dcb9304229e08610d7c1d4ff07155
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2021-12-31 17:18:13 +0500

    Code refactoring and bug fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\User;
 use App\Repository\API\TwitchRepository;
 use DB;
 use Illuminate\Console\Scheduling\Schedule;
@@ -20,8 +21,23 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->call(function () {
             $TwitchRepository = resolve(\App\Repository\API\TwitchRepository::class);
-            $TwitchRepository->getTopLiveStreams();
-        })->everyMinute();
+            //getting Twitch token, refreshtoken from a DB instead of Auth::user() for a current user because due to the task assignment a cronjob should
+            //run this task every 15min and Auth is not available there.
+            $user = User::where('email', 'bakhtiyor.bahriddinov@gmail.com')->first();
+            $TwitchRepository->init([
+                'twitch_url' => env('TWITCH_URL'),
+                'twitch_token_refresh_url' => env('TWITCH_TOKEN_REFRESH_URL'),
+                'token'=>$user->twitch_token,
+                'refresh_token'=>$user->twitch_refresh_token,
+                'client_id'=>env('TWITCH_CLIENT_ID'),
+                'client_secret'=>env('TWITCH_CLIENT_SECRET'),
+                'twitch_id'=>$user->twitch_id
+            ]);
+            $TwitchRepository->fetchTopStreams();
+        })
+        ->name('fetch-top-streams')
+        ->withoutOverlapping(5)
+        ->everyFifteenMinutes();
     }
 
     /**
--- app/Http/Controllers/TwitchController.php
+++ app/Http/Controllers/TwitchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Repository\TwitchRepositoryInterface;
 use Auth;
 
@@ -14,33 +15,28 @@ class TwitchController extends Controller
     public function __construct(TwitchRepositoryInterface $twitchRepository)
     {
         $this->twitchRepository = $twitchRepository;
+
+        //getting Twitch token, refreshtoken from a DB instead of Auth::user() for a current user because due to the task assignment a cronjob should
+        //run this task every 15min and Auth is not available there.
+        $user = User::where('email', 'bakhtiyor.bahriddinov@gmail.com')->first();
+        $this->twitchRepository->init([
+            'twitch_url' => env('TWITCH_URL'),
+            'twitch_token_refresh_url' => env('TWITCH_TOKEN_REFRESH_URL'),
+            'token'=>$user->twitch_token,
+            'refresh_token'=>$user->twitch_refresh_token,
+            'client_id'=>env('TWITCH_CLIENT_ID'),
+            'client_secret'=>env('TWITCH_CLIENT_SECRET'),
+            'twitch_id'=>$user->twitch_id
+        ]);
     }
 
     public function fetchListOfTags()
     {
-        $this->twitchRepository->init([
-            'twitch_url' => env('TWITCH_URL'),
-            'twitch_token_refresh_url' => env('TWITCH_TOKEN_REFRESH_URL'),
-            'token'=>Auth::user()->twitch_token,
-            'refresh_token'=>Auth::user()->twitch_refresh_token,
-            'client_id'=>env('TWITCH_CLIENT_ID'),
-            'client_secret'=>env('TWITCH_CLIENT_SECRET'),
-            'twitch_id'=>Auth::user()->twitch_id
-        ]);
         $this->twitchRepository->fetchListOfTags();
     }
 
     public function fetchTopStreams()
     {
-        $this->twitchRepository->init([
-            'twitch_url' => env('TWITCH_URL'),
-            'twitch_token_refresh_url' => env('TWITCH_TOKEN_REFRESH_URL'),
-            'token'=>Auth::user()->twitch_token,
-            'refresh_token'=>Auth::user()->twitch_refresh_token,
-            'client_id'=>env('TWITCH_CLIENT_ID'),
-            'client_secret'=>env('TWITCH_CLIENT_SECRET'),
-            'twitch_id'=>Auth::user()->twitch_id
-        ]);
         $this->twitchRepository->fetchTopStreams();
     }
 
